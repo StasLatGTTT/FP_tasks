dc6e722763651fb38354a0b842d9920c0dca80c9
Stanislau Latushko <staslatgttt@gmail.com>
2018-07-01 20:34:15 +0300

    First task ready

---
+++ task_1.hs
@@ -0,0 +1,17 @@
+-- Part a)
+list_gen_1 :: Int -> [(Int, Int -> Int)]
+list_gen_1 a = [(x, succ) | x <- [1..a]]
+tmp_list_1 = [ (list_gen_1 x) | x <- [1..3]]
+answer_a :: [([(Int, Int -> Int)], Char)]
+answer_a = [(x, 'ы') | x <- tmp_list_1]
+
+-- Part b)
+answer_b :: [(Char, Int)] -> ([Int], [Char])
+answer_b a = ([ snd x | x <- a], [fst x | x <- a])
+
+-- Part c)
+answer_c a = (a,a)
+
+-- Part d)
+answer_d :: ([Char] -> Char) -> [Char] -> Char
+answer_d f a = f a
