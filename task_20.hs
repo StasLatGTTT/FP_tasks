ad9a87ec40b7d787685b9d557773bc16da412305
Stanislau Latushko <staslatgttt@gmail.com>
2018-11-10 10:05:50 +0300

    improved task 20

--- task_20.hs
+++ task_20.hs
@@ -1,9 +1,11 @@
 primary 0 = False
 primary 1 = False
 primary x = not ( or [mod x i == 0 | i <- [2..(div x 2)]])
-part n = answ
+
+part n | n > 2, mod n 2 == 0  = answ
             where as = filter primary [2..n]
                   bs = [(x, n - x) | x <- as, primary (n - x)]
                   answ = if null bs
                          then (-1, -1)
                          else head bs
+part n = error "Incorrect input"
